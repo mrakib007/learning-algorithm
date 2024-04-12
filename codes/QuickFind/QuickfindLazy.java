2b5f6c12bc4a61b659fb820d94576eb41a02eb6c
Rakib Hasan <mrakibhasan199744@gmail.com>
2024-04-12 17:32:18 +0600

    Quick Union lazy approach

---
+++ codes/QuickFind/QuickfindLazy.java
@@ -0,0 +1,39 @@
+public class QuickfindLazy {
+    private int[] id;
+
+    public QuickfindLazy(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+    public static void main(String[] args) {
+        QuickfindLazy qu = new QuickfindLazy(10);
+        qu.union(4, 3);
+        qu.union(3, 8);
+        qu.union(6, 5);
+        qu.union(9, 4);
+        qu.union(2, 1);
+
+        System.out.println(qu.connected(5, 4));
+        System.out.println(qu.connected(8, 4));
+    }
+}
